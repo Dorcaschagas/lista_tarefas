72f477d87c826b1bfd4ec04c8922cc2551ade74a
Dorcas Chagas <dorcaspereira30@gmail.com>
2023-06-01 23:53:09 -0300

    05

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-document.addEventListener('keypress', function(e){
-    if(e.key === 'Enter'){
-        const btn = document.getElementById('button')
+let ordem = []
 
-        btn.click()
+let botao = document.getElementById('organizar')
+botao.innerHTML = "⁝"
+
+function organizar(){
+    ordem.sort()
+    let elementos = ''
+
+    for(let i=0; i<ordem.length; i++){
+        elementos += "<li class='li'><input class='check' type='checkbox' id='myCheckbox'><label for='myCheckbox'>" + ordem[i] + "</label></li>"
+
+        document.getElementById('lista').innerHTML = elementos
+        console.log(ordem[i])
     }
-
-})
+    // document.getElementById('lista').innerHTML = elementos
+    
+}
 
 function adicionar (){
+    botao.innerHTML = "organizar texto"
 
     let text = document.getElementById('texto').value;
 
     let list = document.getElementById('lista').innerHTML;
-    
-    let ordem = []
 
     if( text == null || text == '') {
         return
     }
-
+    
     list += "<li class='li'><input class='check' type='checkbox' id='myCheckbox'><label for='myCheckbox'>" + text + "</label></li>";
-
+    
     ordem.push(text)
 
     for(let i=0; i < ordem.length; i++){
-        console.log(i + ':' + ordem[i])
+        // console.log(i + ':' + ordem[i])
     }
    
     document.getElementById('lista').innerHTML = list;
 
     document.getElementById('texto').value = null;
-    console.log(ordem)
 }
+
+document.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        const btn = document.getElementById('button')
+        e.preventDefault()
+        btn.click()
+    }
+
+})
+
+botao.addEventListener('click', onmouseover)
