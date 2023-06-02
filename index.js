71a5dc6a54034f5f762edbdb7247fbd982243bce
Dorcas Chagas <dorcaspereira30@gmail.com>
2023-06-02 13:06:39 -0300

    06

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 let ordem = []
-
-let botao = document.getElementById('organizar')
-botao.innerHTML = "⁝"
-
+document.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        const btn = document.getElementById('button')
+        btn.click()
+    }
+})
 function organizar(){
     ordem.sort()
     let elementos = ''
-
+    
     for(let i=0; i<ordem.length; i++){
         elementos += "<li class='li'><input class='check' type='checkbox' id='myCheckbox'><label for='myCheckbox'>" + ordem[i] + "</label></li>"
-
+        
         document.getElementById('lista').innerHTML = elementos
         console.log(ordem[i])
     }
-    // document.getElementById('lista').innerHTML = elementos
     
+}
+function mudarTexto(novoTexto){
+    
+    botao.innerHTML = novoTexto
 }
 
 function adicionar (){
@@ -35,19 +40,18 @@ function adicionar (){
     for(let i=0; i < ordem.length; i++){
         // console.log(i + ':' + ordem[i])
     }
-   
+    
     document.getElementById('lista').innerHTML = list;
-
+    
     document.getElementById('texto').value = null;
 }
 
-document.addEventListener('keypress', function(e){
-    if(e.key === 'Enter'){
-        const btn = document.getElementById('button')
-        e.preventDefault()
-        btn.click()
-    }
 
-})
 
-botao.addEventListener('click', onmouseover)
+let botao = document.getElementById('organizar')
+localStorage.setItem('ordem', JSON.stringify(ordem));
+
+let ordemArmazenada = localStorage.getItem('ordem');
+if (ordemArmazenada) {
+  ordem = JSON.parse(ordemArmazenada);
+}
